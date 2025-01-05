bc2cb35b92ec756c3e19a6ff1b37c36c39dd175d
Tomáš Sýs <119186266+tomas-sys96@users.noreply.github.com>
2025-01-05 11:16:35 +0100

    Solve exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  let reversed = "";
+  const stringLength = string.length;
 
+  for (let i = 0; i < stringLength; i++) {
+    reversed += string[(stringLength - 1) - i];
+  }
+
+  return reversed;
 };
 
 // Do not edit below this line
