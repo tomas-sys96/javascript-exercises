77e9f0d30377d230c8fbbb36c64cad5a779e8bcf
Tomáš Sýs <119186266+tomas-sys96@users.noreply.github.com>
2025-01-05 10:59:53 +0100

    Solve exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(str, reps) {
+  if (reps < 0) {
+    return "ERROR";
+  }
 
+  let concatenatedString = "";
+  for (let i = 0; i < reps; i++) {
+    concatenatedString += str;
+  }
+  return concatenatedString;
 };
 
 // Do not edit below this line
