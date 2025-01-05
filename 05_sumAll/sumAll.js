3d5a1905c223ac4a1a6efea28dc129c1fc512ceb
Tomáš Sýs <119186266+tomas-sys96@users.noreply.github.com>
2025-01-05 14:05:47 +0100

    Solve exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,32 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+  if (!validNumbers(a, b)) {
+    return "ERROR";
+  }
 
+  return (Math.abs(a - b) + 1) * (a + b) / 2;
 };
+
+function isNegative(num) {
+  return num < 0;
+}
+
+function isNonInteger(num) {
+  return Math.floor(num) !== num;
+}
+
+function isNonNumber(num) {
+  return typeof num !== "number";
+}
+
+function validNumbers(a, b) {
+  for (const num of [a, b]) {
+    if (isNegative(num) || isNonInteger(num) || isNonNumber(num)) {
+      return false;
+    }
+  }
+
+  return true;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
